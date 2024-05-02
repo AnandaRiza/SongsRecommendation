15e3554298e4ed5fa8cfb6f299bd03407573dfab
Ananda Riza <87215017+AnandaRiza@users.noreply.github.com>
2024-05-02 09:48:44 +0700

    Initial Commit

---
+++ SongsRecommendation/SongsRecommendationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongsRecommendationApp.swift
+//  SongsRecommendation
+//
+//  Created by MACBOOK PRO on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SongsRecommendationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
